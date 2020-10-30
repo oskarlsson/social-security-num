19dab0620fa60d6a1bcf0c61d4f6a489f127414c
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2020-10-30 16:27:56 +0100

    Simplifyed ValidDay method and added comments

--- Program.cs
+++ Program.cs
@@ -53,12 +53,13 @@ namespace Personnummer3
         }
         static bool ValidAmount(string userInput)
         {
+            //Check if input is 12 numbers
             int amount = 0;
             foreach (char num in userInput)
             {
                 amount += 1;
-            }
-            //Check if input is 12 numbers
+            }           
+            //Returns false if amount of chars in input is not equal to 12
             if (amount != 12)
             {
                 return false;
@@ -70,9 +71,10 @@ namespace Personnummer3
         }
         static bool ValidYear(string userInput)
         {
+            //Picks and converts the year from the input to interger
             string year = userInput.Substring(0, 4);
-            int y = int.Parse(year);
-            
+            int y = int.Parse(year);           
+            //Return true if year is within range
             if (y >= 1753 && y <= 2020)
             {
                 return true;
@@ -84,9 +86,10 @@ namespace Personnummer3
         }
         static bool ValidMonth(string userInput)
         {
+            //Picks and converts the month from the input to interger
             string month = userInput.Substring(4, 2);
             int m = int.Parse(month);
-            
+            //Returns true if month is  within range
             if (m >= 1 && m <= 12)
             {
                 return true;
@@ -98,9 +101,10 @@ namespace Personnummer3
         }       
         static bool IsLeapYear(string userInput)
         {
+            //Picks and converts the year from the input to interger
             string year = userInput.Substring(0, 4);
             int y = int.Parse(year);
-
+            //Returns true if the year is a leap year
             if (y % 400 == 0 || y % 100 != 0 && y % 4 == 0)
             {
                 return true;
@@ -112,17 +116,22 @@ namespace Personnummer3
         }
         static bool ValidDay(string userInput)
         {
+            //Picks the year, month and day to substrings
+            //string year is only used in the IsLeapYear method so it does not require conversion
             string year = userInput.Substring(0, 4);
-            string month = userInput.Substring(4, 2);
-            string day = userInput.Substring(6, 2);           
+            string m = userInput.Substring(4, 2);
+            int month = int.Parse(m);
+            string d = userInput.Substring(6, 2);
+            int day = int.Parse(d);
             
-            //Array of numbers that match userinput
+            //Array of numbers that match userinput for month to the valid amount of days
             int[] month31days = { 1, 3, 5, 7, 8, 10, 12 };
             int[] month30days = { 4, 6, 9, 11 };
             int[] feb = { 2 };
 
+            
             //Day can't be lower than one
-            if (int.Parse(day) < 1)
+            if (day < 1)
             {
                 return false;
             }
@@ -131,9 +140,9 @@ namespace Personnummer3
             //Turning it false if second condition is met
             for (int i = 0; i < month31days.Length; i++)
             {
-                if (month31days[i] == int.Parse(month))
+                if (month31days[i] == month)
                 {
-                    if (int.Parse(day) > 31)
+                    if (day > 31)
                     {
                         return false;
                     }
@@ -141,9 +150,9 @@ namespace Personnummer3
             }
             for (int i = 0; i < month30days.Length; i++)
             {
-                if (month30days[i] == int.Parse(month))
+                if (month30days[i] == month)
                 {
-                    if (int.Parse(day) > 30)
+                    if (day > 30)
                     {
                         return false;
                     }
@@ -151,18 +160,18 @@ namespace Personnummer3
             }
             for (int i = 0; i < feb.Length; i++)
             {
-                if (feb[i] == int.Parse(month))
+                if (feb[i] == month)
                 {
                     if (IsLeapYear(year) == true)
                     {
-                        if (int.Parse(day) > 29)
+                        if (day > 29)
                         {
                             return false;
                         }
                     }
                     else
                     {
-                        if (int.Parse(day) > 28)
+                        if (day > 28)
                         {
                             return false;
                         }
@@ -173,16 +182,16 @@ namespace Personnummer3
         }
         static bool Gender(string userInput)
         {
-            //
+            //Pick and converts the number speciyfing the gender, then is divided by 2 to return outcome.
             string num = userInput.Substring(10, 1);
             int genNum = int.Parse(num);
-            if (genNum % 2 == 0)
+            if (genNum % 2 != 0)
             {
-                return false;
+                return true;
             }
             else
             {
-                return true;
+                return false;
             }
         }
         static void Output(string userInput)
